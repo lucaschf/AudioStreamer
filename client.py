21f47a13f5dd294ed8ce7e057155df1a5197acf0
lucaschfonseca <lucaschfonseca@gmail.com>
2020-11-24 08:50:08 -0300

    initial commit

---
+++ client.py
@@ -0,0 +1,29 @@
+import socket
+
+import pyaudio
+
+server_address = "127.0.0.1"
+server_port = 12000
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((server_address, server_port))
+
+p_audio = pyaudio.PyAudio()
+
+FORMAT = 8
+CHANNELS = 2
+RATE = 44100
+CHUNK = 2048
+
+stream = p_audio.open(format=FORMAT, channels=CHANNELS, rate=RATE, output=True)
+
+content = client_socket.recv(CHUNK)
+
+while content:
+    stream.write(content)  # plays audio
+    content = client_socket.recv(CHUNK)
+
+print("Audio played")
+
+stream.close()
+p_audio.terminate()
+client_socket.close()
---
+++ server.py
@@ -0,0 +1,31 @@
+# coding: utf-8
+
+import socket
+import wave
+
+server_port = 12000
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind(('', server_port))
+server_socket.listen(0)
+
+CHUNK = 2048  # audio frame size
+
+file_name = '../Nokia-Always here (Ghema 127).wav'
+wf = wave.open(file_name, 'rb')
+
+print("server awaiting....")
+
+socket_connection, address = server_socket.accept()
+
+print(type(socket_connection))
+
+print(f"connection established with {address}")
+
+data = wf.readframes(CHUNK)
+
+while data:
+    socket_connection.send(data)
+    data = wf.readframes(CHUNK)
+
+socket_connection.close()
+wf.close()
